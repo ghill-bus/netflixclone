037ad6e1727f2ef957fda80ca52797e94b9ed4e8
Student 37 <student37@Codes-MacBook-Air-8.local>
2023-11-21 15:01:07 -0500

    netflixclone

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCApTxXeM5Rvyp8evVhelWj_kgChEQ2_7c",
+    authDomain: "netflix-clone-e1c1b.firebaseapp.com",
+    projectId: "netflix-clone-e1c1b",
+    storageBucket: "netflix-clone-e1c1b.appspot.com",
+    messagingSenderId: "752124538356",
+    appId: "1:752124538356:web:d9782599b7238b50eedda1"
+  };
